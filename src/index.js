173b21541ec96460d4295b969d4bcc9bbf18c07b
Srinivas Tamada <srinivas@9lessons.info>
2020-04-18 17:04:19 -0400

    Node Server

---
+++ src/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const queries = require('./queries');
+const PORT = process.env.PORT || 3030;
+const app = express();
+
+/* JSON body parse*/
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/* Request validation */
+const { validate, ValidationError, Joi } = require('express-validation');
+app.use(function (err, req, res, next) {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json(err);
+  }
+  return res.status(500).json(err);
+});
+
+app.get('/users', async (req, res, next) => {
+  try {
+    queries.users().then((data) => {
+      res.json(data);
+    });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* ui validation*/
+const uidValidation = {
+    params: Joi.object({
+        uid: Joi.number().integer().required(),
+    }),
+  };
+
+app.get('/user/:uid', validate(uidValidation), async (req, res, next) => {
+  const uid = req.params.uid;
+  try {
+    queries.user(uid).then((data) => {
+      res.json(data);
+    });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* Signup validation*/
+const signupValidation = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    username: Joi.string().min(3).max(20).required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required(),
+  }),
+};
+
+app.post('/signup', validate(signupValidation), async (req, res, next) => {
+  const postData = req.body;
+  try {
+    queries.signup(postData).then((data) => {
+      res.json({ status: 'success' });
+    });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on PORT:', PORT);
+});
---
+++ src/queries.js
@@ -0,0 +1,75 @@
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'node',
+});
+
+const users = async () => {
+  const promise = new Promise((resolve, reject) => {
+    connection.query('SELECT uid, username FROM users', function (
+      error,
+      data,
+      fields
+    ) {
+      if (error) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+  return promise;
+};
+
+const user = async (uid) => {
+  const promise = new Promise((resolve, reject) => {
+    connection.query(
+      'SELECT uid, username, email FROM users WHERE uid = "' + uid + '"',
+      function (error, data, fields) {
+        if (error) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      }
+    );
+  });
+  return promise;
+};
+
+const signup = async (postData) => {
+  const username = postData.username;
+  const email = postData.email;
+  const password = postData.password;
+
+  const salt = bcrypt.genSaltSync();
+  const encryptedPassword = crypt.hashSync(password, salt);
+  const promise = new Promise((resolve, reject) => {
+    connection.query(
+      'INSERT INTO users (username,email,password) VALUES ("' +
+        username +
+        '","' +
+        email +
+        '","' +
+        encryptedPassword +
+        '")',
+      function (error, data, fields) {
+        if (error) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      }
+    );
+  });
+  return promise;
+};
+
+module.exports = {
+  users,
+  user,
+  signup,
+};
