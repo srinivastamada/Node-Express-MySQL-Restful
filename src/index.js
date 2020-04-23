c338300bbaf470afda5e87ade02e2762cb960689
Srinivas Tamada <srinivas@9lessons.info>
2020-04-23 10:32:34 -0400

    M

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/* CORS cross domain 
+*  Replace * with your domain
+*/
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /* Request validation */
 const { validate, ValidationError, Joi } = require('express-validation');
 
